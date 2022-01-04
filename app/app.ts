2e1d38ede3c89d5b6035aaa010752621fcc60f76
hodasalah <hodasalah35@gmail.com>
2022-01-04 16:57:39 +0300

    add index html script src to bundle.js

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,5 @@
 function startGame() {
   let messagesElement = document.getElementById("messages");
-  messagesElement.innerText = "welcome to our game..."
+  messagesElement.innerText = "welcome to multiMath starting new game..."
 }
+document.getElementById('startGame').addEventListener('click', startGame)
